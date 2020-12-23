14640030132466a66eaf56b79457412e6d2e8365
William Wang <bn.william.w@gmail.com>
2020-12-23 00:38:51 -0500

    Add files via upload

--- python_test_quiz.py
+++ python_test_quiz.py
@@ -1,48 +1,52 @@
+"""
+Name: William Wang
+
+Python Test Quiz where each answer has specified weights
+"""
+
+
+
 class Question:
-    def __init__(self, prompt, questions, options, answer_values):
+    def __init__(self, prompt, questions, inputs, answer_values):
         self.prompt = prompt
-        self.questions = questions
         self.options = options
+        self.inputs = inputs
         self.answer_values = answer_values
 
 prompt = ["Have you felt chest pain in the past week?",
           "Have you experienced dizziness in the past week?"]
 
-questions = ["Yes, on multiply occasions", "Yes, sometimes", "Yes, but rarely", "No"]
+options = ["Yes, on multiply occasions", "Yes, sometimes", "Yes, but rarely", "No"]
 
-options = ["a", "b", "c", "d"]
+inputs = ["a", "b", "c", "d"]
 
 answer_values = ["1", "0.75", "0.5", "0"]
 
-q1 = Question(prompt[0], questions, options, answer_values)
-q2 = Question(prompt[1], questions, options, answer_values)
+q1 = Question(prompt[0], options, inputs, answer_values)
+q2 = Question(prompt[1], options, inputs, answer_values)
 
 question_list = [q1, q2]
-
-
-
 
 def get_answer(question):
     print(question.prompt)
     i = 0
-    for question_option in questions:
-        text = "(" + options[i] + ") " + question_option
+    for option in options:
+        text = "(" + inputs[i] + ") " + option
         print(text)
         i += 1
 
     answer = input()
+
     try:
-         option_index = options.index(answer)
-         return answer_values[option_index]
+         input_index = inputs.index(answer)
+         return answer_values[input_index]
     except:
          print("\"" + answer + "\" is an invalid option. Please try again")
-         return try_q(question)
-
-
+         return get_answer(question)
 
 def run_quiz(questions):
     print("Please enter the lowercase letter specified for each option")
-    print("For example, if your chosen option is \"(a)\", enter \"a\"\n")
+    print("For example, if your chosen option is \"(a)\", please enter \"a\"\n")
 
     score = 0
 
@@ -53,12 +57,15 @@ def run_quiz(questions):
 
     print("Your risk score is " + str(risk_score) + "%")
 
-    if risk_score >= 75:
-        print("You are potentially at high risk. Please contact a medical professional immediately.")
-    elif 25 < risk_score < 75:
-        print("You are potentially at risk. Please contact a medical professional in the near future.")
+
+    if risk_score >= 90:
+         print("You are at very high risk. Please contact a medical professional immediately.")
+    elif 65 <= risk_score < 90:
+         print("You are potentially at a high risk. Please contact a medical professional as soon as possible.")
+    elif 25 <= risk_score < 65:
+         print("You are potentially at risk. Please contact a medical professional in the near future.")
     else:
-        print("You are not at risk. Contact a medical professional for a more reliable diagnosis.")
+         print("You are not at risk. Contact a medical professional for a more reliable diagnosis.")
 
 
 run_quiz(question_list)
