494fe4681916802e705587c4d7a332ff55422ddc
William Wang <bn.william.w@gmail.com>
2020-12-25 16:15:40 -0500

    Name change

--- python_quiz.py
+++ python_quiz.py
@@ -3,26 +3,9 @@ New Python Quiz without explicit dictionary
 
 '''
 
-class Question:
-    def __init__(self, prompt, options, values):
-        self.prompt = prompt
-        self.options = options
-        self.values = values
+from question_database import question_list
 
 
-prompt = ["Have you felt chest pain in the past week?",
-          "Have you experienced dizziness in the past week?",
-          "Have you experienced shortness of breath in the past week?"]
-
-options = ["Yes, on multiply occasions", "Yes, sometimes", "Yes, but rarely", "No"]
-
-values = [1, 0.75, 0.5, 0]
-
-q1 = Question(prompt[0], options, values)
-q2 = Question(prompt[1], options, values)
-q3 = Question(prompt[2], options, values)
-
-question_list = [q1, q2, q3]
 
 def get_answer(question):
     print(question.prompt)  # print question
---
+++ question_database.py
@@ -0,0 +1,23 @@
+'''
+Question Database File
+'''
+
+class Question:
+    def __init__(self, prompt, options, values):
+        self.prompt = prompt
+        self.options = options
+        self.values = values
+
+prompt = ["Have you felt chest pain in the past week?",
+          "Have you experienced dizziness in the past week?",
+          "Have you experienced shortness of breath in the past week?"]
+
+options = ["Yes, on multiply occasions", "Yes, sometimes", "Yes, but rarely", "No"]
+
+values = [1, 0.75, 0.5, 0]
+
+q1 = Question(prompt[0], options, values)
+q2 = Question(prompt[1], options, values)
+q3 = Question(prompt[2], options, values)
+
+question_list = [q1, q2, q3]
