71c38cac3d936b40c93a4ae297d3ff9aa519b53e
William Wang <bn.william.w@gmail.com>
2020-12-24 22:18:25 -0500

    Update test_quiz_dictionary_.py

--- test_quiz_dictionary_.py
+++ test_quiz_dictionary_.py
@@ -3,7 +3,7 @@ class Question:
         self.prompt = prompt
         self.options = options
         self.inputs = inputs
-       #self.answer_values = answer_values
+
 
 prompt = ["Have you felt chest pain in the past week?",
           "Have you experienced dizziness in the past week?"]
@@ -12,9 +12,9 @@ options = ["Yes, on multiply occasions", "Yes, sometimes", "Yes, but rarely", "N
 
 inputs = {
     "a": 1,
-     "b": 0.75, 
-     "c": 0.5, 
-     "d": 0
+    "b": 0.75,
+    "c": 0.5,
+    "d": 0
 }
 
 q1 = Question(prompt[0], options, inputs)
@@ -23,7 +23,7 @@ q2 = Question(prompt[1], options, inputs)
 question_list = [q1, q2]
 
 def get_answer(question):
-    print(question.prompt)
+    print(question.prompt)  # print question
     i = 0
     for key in inputs.keys():
         text = "(" + str(key) + ") " + options[i]
@@ -33,11 +33,11 @@ def get_answer(question):
     answer = input()
 
     try:
-         if answer in inputs:
+        if answer in inputs:
             return inputs[str(answer)]
     except:
-         print("\"" + answer + "\" is an invalid option. Please try again")
-         return get_answer(question)
+        print("\"" + answer + "\" is an invalid option. Please try again")
+        return get_answer(question)
 
 def run_quiz(questions):
     print("Please enter the lowercase letter specified for each option")
@@ -54,13 +54,13 @@ def run_quiz(questions):
 
 
     if risk_score >= 90:
-         print("You are at very high risk. Please contact a medical professional immediately.")
+        print("You are at very high risk. Please contact a medical professional immediately.")
     elif 65 <= risk_score < 90:
-         print("You are potentially at a high risk. Please contact a medical professional as soon as possible.")
+        print("You are potentially at a high risk. Please contact a medical professional as soon as possible.")
     elif 25 <= risk_score < 65:
-         print("You are potentially at risk. Please contact a medical professional in the near future.")
+        print("You are potentially at risk. Please contact a medical professional in the near future.")
     else:
-         print("You are not at risk. Contact a medical professional for a more reliable diagnosis.")
+        print("You are not at risk. Contact a medical professional for a more reliable diagnosis.")
 
 
 run_quiz(question_list)
