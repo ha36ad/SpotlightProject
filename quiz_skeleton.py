b2f398a1d469b03a2f2f9dd6cbae86bc8230ae3a
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-12-27 18:08:59 -0500

    Update quiz_skeleton.py

--- quiz_skeleton.py
+++ quiz_skeleton.py
@@ -1,26 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 from question_database_tuples import question_list
-
-# ideally if we can have this risk function go to the database for easier maintenace
-def risk_print(risk_score):
-    print("Your risk score is " + str(round(risk_score, rounding_decimal)) + "%")
-
-    if risk_score >= 90:
-        print("You are at very high risk. Please contact a medical professional immediately.")
-    elif 70 <= risk_score < 90:
-        print("You are potentially at a high risk. Please contact a medical professional as soon as possible.")
-    elif 50 <= risk_score < 70:
-        print("You are potentially at moderate risk. "
-              "Please contact a medical professional as soon as it is convenient.")
-    elif 25 <= risk_score < 50:
-        print("You are potentially at risk. Please contact a medical professional in the near future.")
-    else:
-        print("You are not at risk. Contact a medical professional for a more reliable diagnosis.")
 
 #constants
 HEIGHT= 800
 WIDTH = 800
-rounding_decimal = 2
 
 root=tk.Tk()
 
@@ -31,24 +15,23 @@ def onClick(answer):
     global next_question
 
     answer_list.append(float(answer))
-    if next_question < len(question_list):
+    if next_question < len(qd.question_list):
         #clear second frame so new options are generated
         for child in frame2.winfo_children():
             child.destroy()
 
         #Display next question
-        question.config(text=question_list[next_question].prompt)
+        question.config(text=qd.question_list[next_question].prompt)
 
-        #Recreate options and next button
+        #Recreate options, next button, and progress bar showing progress
         create_options(next_question)
         create_next()
-
         next_question += 1
+        show_progress(next_question/len(qd.question_list)*100)
 
     else:
-        risk_score = (sum (answer_list) * 100) / len(question_list)
-
-        risk_print(risk_score)
+        risk_score = (sum (answer_list) * 100) / len(qd.question_list)
+        qd.risk_print(risk_score)
 
 #Canvas
 canvas=tk.Canvas(root,height = HEIGHT, width = WIDTH)
@@ -63,6 +46,7 @@ frame2.place (relx = 0.1,rely = 0.1, relwidth = 0.8, relheight = 0.8)
 #Intialization of variables
 answer_list = []
 next_question = 1
+index = tk.StringVar(value = "1")
 
 #Next Button
 def create_next():
@@ -70,20 +54,22 @@ def create_next():
     next_button.pack()
 
 #Question
-question= tk.Label(frame, font = ("Gotham", 16), width = 500,justify = "center", wraplength = 400,
-                   text = question_list[0].prompt)
+question= tk.Label(frame, font = ("Gotham", 16), width = 500,justify = "center", wraplength = 400, text = qd.question_list[0].prompt) 
 question.pack()
 
-#Buttons
-index = tk.StringVar(value = "1")
+#Progress bar
+def show_progress(number):
+    progress_bar = tk.ttk.Progressbar(frame2, orient = tk.HORIZONTAL, length = 100, mode ='determinate', value = number)
+    progress_bar.pack(pady=20,padx = 10)
 
+#Create options
 def create_options(i):
-    for text, option in question_list[i].options:
-        tk.Radiobutton(frame2, text = text ,font = ("Gotham",14),
-                       background = 'gray',padx = 10, pady = 10, variable = index, value = option).pack()
+    for text, option in qd.question_list[i].options:
+        tk.Radiobutton(frame2, text = text ,font = ("Gotham",14),background = 'gray',padx = 10, pady = 10, variable = index, value = option).pack()
 
-    
+#Create the progres bar,  the first question's options, and the progress bar    
 create_options(0)
+show_progress(0)
 create_next()
 
 root.mainloop()
